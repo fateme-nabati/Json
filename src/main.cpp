cc00ed6b835190e83dc62da83c4e7822cdddf52c
Mina Yaghoubi <MinaYaghoubimansour@gmail.ir>
2021-01-01 21:40:18 +0330

    add main.cpp with branche 1

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include "../header/json.hpp"
+using namespace std;
+int main()//The program starts from the main function
+{
+    for (;;)
+    {
+        string MainCommand, OtherCommands;
+        string InputCommand;
+        cout << "->";
+        string str;
+        getline(cin, str);
+        while (str != "</>") //Data is retrieved from the input until the program reaches </>
+        {
+            InputCommand += str;
+            InputCommand += '\n';
+            getline(cin, str);
+        }
+        MainCommand = "";
+        int count = 0;
+        while (InputCommand[count] != '\n' && InputCommand[count] != ' ' && InputCommand[count] && InputCommand[count] != '{' && InputCommand[count] != '[' && InputCommand[count] != '\t')
+        {
+            MainCommand += InputCommand[count];
+            ++count;
+        }
+        while (InputCommand[count])
+        {
+            OtherCommands += InputCommand[count];
+            ++count;
+        }
+        if (MainCommand == "exit") //Exit the program
+        {
+            cout << "I hope you enjoyed my program" << endl;
+            break;
+        }
+        commandFunc(MainCommand, OtherCommands); //call commandFunc function
+    }
+
+    return 0;
+}
